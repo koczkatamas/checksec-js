9f7385e9cf11f17364396e3051105b0e9188c82d
KT <koczkatamas@gmail.com>
2018-03-24 22:55:47 +0100

    Implement ELF checks

--- index.js
+++ index.js
@@ -6,11 +6,33 @@ const files = fs.readdirSync("examples").filter(x => !x.endsWith(".sh") && !x.en
 for (const file of files) {
     const fileContent = fs.readFileSync(`examples/${file}`);
     const parsedElf = new Elf(new KaitaiStream(fileContent));
+    const prgHdrs = parsedElf.header.programHeaders;
+
     console.log(`${file}: ${Elf.ObjType[parsedElf.header.eType]} ${Elf.Machine[parsedElf.header.machine]} ${Elf.Bits[parsedElf.bits]} ${Elf.Endian[parsedElf.endian]} ${Elf.OsAbi[parsedElf.abi]} v${parsedElf.eiVersion}`);
-    const prgHdrTypes = parsedElf.header.programHeaders.map(x => Elf.PhType[x.type]);
-    const sectionHdrNames = parsedElf.header.sectionHeaders.map(x => x.name);
-    console.log(`  prgHdrTypes: ${prgHdrTypes.join(", ")}`);
-    console.log(`  sectionHdrNames: ${sectionHdrNames.join(", ")}`);
-    console.log(`  RELRO: `);
+
+    const dynamicEntries = prgHdrs.map(x => x.dynamic).filter(x => x);
+    if (dynamicEntries.length > 1)
+        console.error("Multiple DYNAMIC entries found. Result can be inconsistent.");
+    const dynamic = dynamicEntries[0].entries || [];
+    const flags1 = dynamic.map(x => x.flag1Values).filter(x => x)[0] || {};
+
+    const bindNow = dynamic.some(x => x.tag === Elf.DynamicArrayTags.BIND_NOW);
+    if (bindNow !== (flags1.now||false))
+        console.error(`Full RELRO test gave inconsistent results (BIND_NOW = ${bindNow}, DT_FLAGS_1.NOW = ${flags1.now}).`);
+
+    const relro = prgHdrs.some(x => x.type === Elf.PhType.GNU_RELRO);
+    const execStack = prgHdrs.find(x => x.type === Elf.PhType.GNU_STACK).flagsObj.execute;
+    const stackCanaryHeur = parsedElf.header.sectionHeaders.filter(x => x.strtab)
+        .some(x => x.strtab.entries.some(y => y.includes("__stack_chk_fail")));
+    const rpath = dynamic.some(x => x.tag === Elf.DynamicArrayTags.RPATH);
+    const runpath = dynamic.some(x => x.tag === Elf.DynamicArrayTags.RUNPATH);
+
+    console.log(`  RELRO: ${!relro ? "No" : bindNow ? "Full" : "Partial"}`);
+    console.log(`  PIE: ${flags1.pie||false}`);
+    console.log(`  NX: ${!execStack}`);
+    console.log(`  Stack canary: ${stackCanaryHeur}`);
+    console.log(`  RPATH: ${rpath}`);
+    console.log(`  RUNPATH: ${runpath}`);
+    console.log("");
     //console.log(JSON.stringify(parsedElf, function(key, value) { return key.startsWith('_') ? undefined : value; }, 4));
 }
